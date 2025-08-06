b38b1feb2e4df5c1eaa4ac30a49fd3b40a819b20
Julio Oliveira <juliooliveira462@gmail.com>
2025-08-06 02:32:08 -0300

    fix alterando dados do banco do deploy

--- src/main/java/com/sop/financeiro/config/SecurityConfig.java
+++ src/main/java/com/sop/financeiro/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -82,11 +83,11 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-            configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            configuration.setAllowedOriginPatterns(List.of("*")); // <- permite qualquer origem
+            configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
             configuration.setAllowCredentials(true);
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
             source.registerCorsConfiguration("/**", configuration);
             return source;
         }
